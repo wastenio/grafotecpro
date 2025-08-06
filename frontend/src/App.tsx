6b7bc952b307670b22d6738b22aa1467ab9f387f
wastenio da silva rocha <wastenio.silva@gmail.com>
2025-08-06 16:41:24 -0300

    Configuração inicial do frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold text-blue-600 mt-10">
+      Tailwind funcionando manualmente!
+    </div>
+  );
+}
+
+export default App;
